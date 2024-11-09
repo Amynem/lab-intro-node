d803132f30fbed5f7e732c82d03dc498412b0e40
Amynem <amy.lauren98@hotmail.com>
2024-11-09 11:30:29 +0100

    lab hecho

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= 0 && pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if( this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for( let i = 0; i < this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
   avg() {}
 }
